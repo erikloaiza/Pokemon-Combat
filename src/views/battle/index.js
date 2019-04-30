f69476355e199b679fb3eccca0fa7127a17f5641
Erik Loaiza <erik.loaiza@uao.edu.co>
2019-04-29 20:35:10 -0500

    Added routes

---
+++ src/views/battle/index.js
@@ -0,0 +1,16 @@
+import React,{Component} from'react'
+import http from 'http'
+
+class Battle extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>Pokemon will be loaded here</div>
+        );
+    }
+} 
+
+export default Battle;
+
