c0c4c5b0d63bf9845c30c4750273b1e81a1a4923
Erik Loaiza <erik.loaiza@uao.edu.co>
2019-05-01 16:02:04 -0500

    Added combat interaction

--- src/views/battle/index.js
+++ src/views/battle/index.js
@@ -12,23 +12,29 @@ class Battle extends Component{
         super(props)
         this.state ={
             enemy:{},
-            player:{}
+            player:{},
+            message:'Selecciona tu próximo ataque',
+            isOnGoingAction:false
         }
     }
     render(){
         const enemy = this.state.enemy
         const player = this.state.player
+        const isOnGoingAction = this.state.isOnGoingAction;
         return(
             <div className="battle-wrapper">
+              <div id='preventClicks' style={isOnGoingAction?{'display':'block'}:{'display':'none'}}></div>
               <audio src={Music} autoPlay loop></audio>
               <audio src={MenuItem} id="Sound-item"></audio>
               <Player desc={enemy} isOponent/>
               <Player desc={player}/>
               <div className="attacks-gui">
-                    <h1>Select next attack</h1>
+                    <h1>{this.state.message}</h1>
                     <div className="attacks">
                         {player.arAtacks?player.arAtacks.map(attack=>{
-                            return <div className="attack" onMouseOver={this.PlayMenuSound}
+                            return <div className="attack" 
+                            onMouseOver={this.playMenuSound}
+                            onClick={this.attackEnemy.bind(this,attack)}
                             >
                             {attack.stAtckName}</div>
                         }):''}
@@ -58,9 +64,64 @@ class Battle extends Component{
             console.log("Error: " + err.message);
           });
     }
-    PlayMenuSound(){
+    playMenuSound(){
       var sound = document.getElementById('Sound-item');
       sound.play();
+    }
+    attackEnemy(attack){
+      const damage = attack.nuAtckPower;
+      const name = attack.stAtckName;
+
+      const {enemy}={...this.state}
+
+      const def = enemy.nuDf;
+    
+      const totalDamage = (damage-def>0)?damage-def:5;
+
+      this.setState({
+        message: this.state.player.stName+' usó '+name,
+        isOnGoingAction:true
+      })
+      setTimeout(()=>{
+          enemy.nuLife -=totalDamage;
+          this.setState({
+            enemy:enemy
+          })
+        if(totalDamage==5)setTimeout(this.setState({message:"No es muy efectivo"}),1000)
+        setTimeout(this.attackPlayer.bind(this),2000);
+      },1000);
+      
+    }
+    attackPlayer(){
+      const attacks = this.state.enemy.arAtacks
+      const attack = attacks[Math.floor(Math.random()*attacks.length)];
+      const damage = attack.nuAtckPower;
+      const name = attack.stAtckName;
+
+      const {player}={...this.state}
+
+      const def = player.nuDf;
+    
+      const totalDamage = (damage-def>0)?damage-def:5;
+
+      this.setState({
+        message:this.state.enemy.stName+' usó '+name
+      })
+
+      setTimeout(()=>{
+        player.nuLife -=totalDamage;
+        this.setState({
+          player:player
+        })
+        if(totalDamage==5)setTimeout(this.setState({message:"No es muy efectivo"}),1000)
+        setTimeout(()=>{
+          this.setState({
+            message:'Selecciona tu próximo ataque',
+            isOnGoingAction:false
+          })
+        },1000);
+      },2000);
+      
     }
 } 
 
