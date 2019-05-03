c0f828f043cdc3a9f817867da6bc49e1aa51368d
Erik Loaiza <erik.loaiza@uao.edu.co>
2019-05-03 10:10:03 -0500

    Added production for hosting
    Removed from .gitignore

---
+++ build/precache-manifest.565ab75f8d18e50d4ef29f20b03b1544.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d267b6ce752dca812f875d0abc7dbb07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1423c124d69c7148ea45",
+    "url": "/static/css/main.55f5fe17.chunk.css"
+  },
+  {
+    "revision": "7b5a49575f456d162961",
+    "url": "/static/js/2.d58a9b30.chunk.js"
+  },
+  {
+    "revision": "1423c124d69c7148ea45",
+    "url": "/static/js/main.38a28b50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7c1c64de7f048541c9fc022aa4825596",
+    "url": "/static/media/hurt-effective.7c1c64de.wav"
+  },
+  {
+    "revision": "14ae23eca4848f6f00e7eef5737e5f69",
+    "url": "/static/media/hurt-ineffective.14ae23ec.wav"
+  },
+  {
+    "revision": "fc02613ee55e33b8c93f90a69be929b6",
+    "url": "/static/media/menu-item.fc02613e.wav"
+  },
+  {
+    "revision": "f502ab8c0330bcfb078c8090e6e86d6d",
+    "url": "/static/media/music.f502ab8c.mp3"
+  },
+  {
+    "revision": "7cc65323fed8223b02acee21dc465294",
+    "url": "/static/media/plattform.7cc65323.png"
+  }
+]);
